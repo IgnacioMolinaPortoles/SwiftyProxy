98e9bde9e8335e002cf011adbd97e2816b942ba2
Ignacio Molina Portoles <inmolinaportoles@teco.com.ar>
2024-08-26 19:31:20 -0300

    Initial Commit

---
+++ SwiftyProxy/SwiftyProxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyProxyApp.swift
+//  SwiftyProxy
+//
+//  Created by Ignacio Molina Portoles on 26/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyProxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
